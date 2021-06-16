93bc9e634fd25a4a51c5d2e55c90d53b33048813
Jorge <jorgecokeb@gmail.com>
2021-06-16 16:53:32 -0400

    fix: opciones del Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,53 @@
-import { Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Colegio Coquimbano
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <a  class="btn" role="button" href="/alumno">
-            Iniciar Sesion
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/home">
+            Colegio Coquimbano
           </a>
-          <button type="button" class="btn">
-            Registrarse
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
+          <Switch>
+            <Route path="/home">
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <a  class="btn" role="button" href="/apoderado">
+                  Apoderado
+                </a>
+                <a  class="btn" role="button" href="/profesor">
+                  Profesor
+                </a>
+                <a  class="btn" role="button" href="/alumno">
+                  Alumno
+                </a>
+                <a  class="btn" role="button" href="/establecimiento">
+                  Establecimiento
+                </a>
+                <a  class="btn" role="button" href="/funcionario">
+                  Funcionario
+                </a>
+              </div>
+            </Route>
+          </Switch>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    </Router>
+ );
 }
 
 export default navbar;
